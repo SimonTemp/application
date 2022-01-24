cc96f9f1d5a798f645ef5cd015f34b7d53882d66
SimonTemp <thukusimon5@gmail.com>
2022-01-24 17:38:01 +0300

    cors

--- validator-service/src/main/java/com/validator/validatorservice/util/WebConfig.java
+++ validator-service/src/main/java/com/validator/validatorservice/util/WebConfig.java
@@ -1,17 +1,22 @@
 package com.validator.validatorservice.util;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("*");
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*");
+            }
+        };
     }
 }
